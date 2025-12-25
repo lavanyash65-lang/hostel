bd9b7a2781386131bfc920550b746718e338a05c
lavanyash65-lang <lavanyash65@gmail.com>
2025-12-25 20:46:54 +0530

    Refactor hostel.py for input handling and priority logic

--- hostel.py
+++ hostel.py
@@ -1,35 +1,30 @@
 import sys
 
-def allocate_room(name,year,cgpa,distance):
-    if cgpa>=8.5 and distance>=200:
-        return "High Priority"
-    elif cgpa>=7.0:
-        return "Medium Priority"
-    else:
-        return "Low Priority"
-
-use_args=False
 if len(sys.argv)==5:
-    try:
-        name=sys.argv[1]
-        year=sys.argv[2]
-        cgpa=float(sys.argv[3])
-        distance=int(sys.argv[4])
-        use_args=True
-    except:
-        use_args=False
-
-if not use_args:
-    name="Anita"
-    year="2nd Year"
+    script_name=sys.argv[0]
+    name=sys.argv[1]
+    year=sys.argv[2]
+    cgpa=float(sys.argv[3])
+    distance=int(sys.argv[4])
+    print("user provided input values:")
+else:
+    script_name=sys.argv[0]
+    name="Lavanya"
+    year="3"
     cgpa=8.2
-    distance=180
+    distance=120
+    print("no input given - using default values:")
 
-priority=allocate_room(name,year,cgpa,distance)
+if cgpa>=8.5 and distance>=100:
+    priority="High Priority"
+elif cgpa>=7.0:
+    priority="Medium Priority"
+else:
+    priority="Low Priority"
 
-print("\n--- Hostel Room Allocation Result ---")
-print("Student Name :",name)
-print("Year         :",year)
-print("CGPA         :",cgpa)
-print("Distance     :",distance)
-print("Priority     :",priority)
+print("script name:",script_name)
+print("student name:",name)
+print("year:",year)
+print("cgpa:",cgpa)
+print("distance:",distance)
+print("priority:",priority)
